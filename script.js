a8a77413f69f0b2839d17fb6057c75aac5271f12
“henry”-bernardin <henry.bernardin@gmail.com>
2023-02-14 23:09:49 -0500

    created getComputerChoice function

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-let choices = ["Paper","Rock","Scissors"];
-let rand = [Math.floor(Math.random()*choices.length)];
-let shoot = choices[rand];
 
-function getComputerChoice(){
-    return shoot;
+// function which randomly selects rock paper or scissors
+// this function uses a switch statement to select the choices
+function getComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * 3);
+    switch(randomChoice) {
+        case 0:
+         return 'Rock'
+        case 1:
+          return 'Scissors'
+        case 2:
+          return 'Paper'
+      }
 }
-getComputerChoice()
+
