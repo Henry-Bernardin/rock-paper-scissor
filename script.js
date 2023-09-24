fe23a39463cae42902bebfaed23ce06d98baab1f
“henry”-bernardin <henry.bernardin@gmail.com>
2023-09-24 09:53:49 -0400

    lowercased the computerSelection results

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * 3);
   switch (randomChoice) {
     case 0:
-      return "Rock";
+      return "rock";
     case 1:
-      return "Scissors";
+      return "acissors";
     case 2:
-      return "Paper";
+      return "paper";
   }
 }
 
