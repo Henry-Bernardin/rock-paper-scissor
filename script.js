6d094214bc1ac3952644faa224c28d03ebf7eef3
Henry Bernardin <henry.bernardin@gmail.com>
2023-12-28 22:18:18 -0500

    Updated corrected function game

--- script.js
+++ script.js
@@ -11,31 +11,31 @@ function getComputerChoice() {
   }
 }
 
-function game(){
-  function playRound(playerSelection, computerSelection) {
-    // Make the player's selection case-insensitive
-    playerSelection = playerSelection.toLowerCase();
-  
-    // Define the possible choices
-    const choices = ['rock', 'paper', 'scissors'];
-  
-    // Check if the player's selection is valid
-    if (!choices.includes(playerSelection)) {
-        return "Invalid choice. Please choose rock, paper, or scissors.";
-    }
-    // Determine the winner of the round
-    if (playerSelection === computerSelection) {
-        return `It's a tie! Both chose ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`;
-    } else if (
-        (playerSelection === 'rock' && computerSelection === 'scissors') ||
-        (playerSelection === 'paper' && computerSelection === 'rock') ||
-        (playerSelection === 'scissors' && computerSelection === 'paper')
-    ) {
-        return `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
-    } else {
-        return `You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`;
-    }
+function playRound(playerSelection, computerSelection) {
+  // Make the player's selection case-insensitive
+  playerSelection = playerSelection.toLowerCase();
+
+  // Define the possible choices
+  const choices = ['rock', 'paper', 'scissors'];
+
+  // Check if the player's selection is valid
+  if (!choices.includes(playerSelection)) {
+      return "Invalid choice. Please choose rock, paper, or scissors.";
   }
+
+  // Determine the winner of the round
+  if (playerSelection === computerSelection) {
+      return `It's a tie! Both chose ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`;
+  } else if (
+      (playerSelection === 'rock' && computerSelection === 'scissors') ||
+      (playerSelection === 'paper' && computerSelection === 'rock') ||
+      (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+      return `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
+  } else {
+      return `You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`;
+  }
+}
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
